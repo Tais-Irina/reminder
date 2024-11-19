3914616c8dc907c9bba4c382f3dcc9649938b82e
Irina Samoylova <novikova.i.v@gmail.com>
2024-11-19 10:21:40 +0300

    Определена функция для срабатывания таймера t. Переменная t теперь глобальная

--- napominalka.py
+++ napominalka.py
@@ -5,20 +5,42 @@ import datetime
 import pygame
 import time
 
+
+
 def set_remind():
+    # время напоминания
+    global t
     rem = sd.askstring('Время напоминания','Введите время напоминания в формате чч:мм')
     if rem:
         try:
             hour = int(rem.split(':')[0])
             minute = int(rem.split(':')[1])
             now = datetime.datetime.now()
-            print(hour, minute, now)
+            #print(hour, minute, now)
             dt = now.replace(hour = hour, minute = minute, second=0)
-            print(dt)
+            #print(dt)
+
             t=dt.timestamp()
             label.config(text = f"Напоминание установлено на {hour:02} : {minute:02}")
         except ValueError:
             mb.showerror("Ошибка", "Неправильный формат времени")
+
+# каждые 10с проверка наступления времени
+def check():
+    global t
+    if t:
+        now = time.time()
+        if now >= t:
+            #play_snd()
+            t = None
+    # рекурсия - сама себя вызывает
+    window.after(10000,check)
+
+
+#переменные глобальные
+t = None
+
+
 
 window = Tk()
 window.title("Reminder")
@@ -29,5 +51,5 @@ label.pack()
 set_button = Button(text = 'Установить напоминание', font = ('Georgia', 15, "bold"),
                     command = set_remind)
 set_button.pack()
-
+check()
 window.mainloop()

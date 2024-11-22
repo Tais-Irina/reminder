4cd0633effc33e124ac3e5dd5b160207902dc4f1
Irina Samoylova <novikova.i.v@gmail.com>
2024-11-22 10:19:34 +0300

    Добавлено окно с напоминанием и основное окно окно программы сделано всегда поверх всех окон

--- napominalka.py
+++ napominalka.py
@@ -5,8 +5,9 @@ import datetime
 import pygame
 import time
 
-
+napominanie=''
 def set_remind():
+    global napominanie
     # время напоминания
     global t
     rem = sd.askstring('Время напоминания',
@@ -19,11 +20,14 @@ def set_remind():
             dt = now.replace(hour = hour, minute = minute, second=0)
             # формат понятный пайтону.
             t=dt.timestamp()
-            text = sd.askstring('Текст напоминания',
+
+            napominanie = sd.askstring('Текст напоминания',
                                 'Введите текст напоминания')
-            label.config(text = f"Напоминание {hour:02} : {minute:02} о {text}")
+            label.config(text = f"Напоминание {hour:02} : {minute:02} о {napominanie}")
         except ValueError:
             mb.showerror("Ошибка", "Неправильный формат времени")
+        except Exception:
+            mb.showerror("Ошибка","Неправильный формат времени")
 
 # каждые 10с проверка наступления времени
 def check():
@@ -32,6 +36,8 @@ def check():
         now = time.time()
         if now >= t:
             play_snd()
+            t_napominanie = datetime.datetime.fromtimestamp(t).strftime("%H:%M")
+            mb.showinfo('Напоминание',f'{t_napominanie} {napominanie} ')
             t = None
     # рекурсия - сама себя вызывает
     window.after(10000,check)
@@ -59,15 +65,16 @@ music_plaing = False
 
 window = Tk()
 window.title("Reminder")
+window.geometry("550x170")
+window.attributes("-topmost",True)
+label = Label(text = 'Установите напоминание', font = ('Georgia', 15, "bold"))
+label.pack(padx=20,pady = 20)
 
-label = Label(text = 'Установите напоминание', font = ('Georgia', 20, "bold"))
-label.pack(pady = 10)
-
-set_button = Button(text = 'Установить напоминание', font = ('Georgia', 15, "bold"),
+set_button = Button(text = 'Установить напоминание', font = ('Georgia', 10, "bold"),
                     command = set_remind)
 set_button.pack(pady = 10)
 
-stop_button = Button(text = 'Остановить музыку', font = ('Georgia', 15, "bold"),
+stop_button = Button(text = 'Остановить музыку', font = ('Georgia', 10, "bold"),
                     command = stop_music)
 stop_button.pack(pady=10)
 check()
